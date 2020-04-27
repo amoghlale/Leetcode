70be0c9516006ee85f797360178a41fb6755e48f
Amogh Lale <amoghlale@gmail.com>
2020-04-27 12:39:49 +0100

    Length of Last word: return 0 if no last word exists

---
+++ Solutions/LengthOfLastWord.py
@@ -0,0 +1,7 @@
+class Solution(object):
+    def lengthOfLastWord(self, s):
+        """
+        :type s: str
+        :rtype: int
+        """
+        return 0 if len(s.split()) == 0 else len(s.split()[-1])
