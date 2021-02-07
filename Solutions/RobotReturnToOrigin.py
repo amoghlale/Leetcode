0bac946f24e4d58aebd9dc35406765eacc707dbc
Amogh Lale <amoghlale@gmail.com>
2021-02-07 18:48:48 +0530

    Robot return to origin problem

---
+++ Solutions/RobotReturnToOrigin.py
@@ -0,0 +1,15 @@
+class Solution:
+    def judgeCircle(self, moves: str) -> bool:
+        x, y = 0, 0
+        for move in moves:
+            if move == 'U':
+                y += 1
+            elif move == 'D':
+                y -= 1
+            elif move == 'R':
+                x += 1
+            else:
+                x -= 1
+        if x == 0 and y == 0:
+            return True
+        return False
