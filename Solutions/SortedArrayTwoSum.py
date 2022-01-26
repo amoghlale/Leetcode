5a7b7b21a577303325ac8fc83b081ccd0ead0db6
Amogh Lale <amoghlale@gmail.com>
2022-01-26 11:46:40 +0530

    Faster solution for sorted array two sum

--- Solutions/SortedArrayTwoSum.py
+++ Solutions/SortedArrayTwoSum.py
@@ -1,3 +1,16 @@
+Solution 1: using 2 pointers i and j (faster than 99.30% of solutions)
+class Solution:
+    def twoSum(self, numbers: List[int], target: int) -> List[int]:
+        i, j = 0, len(numbers) - 1
+        while i <= j:
+            if numbers[i] + numbers[j] == target:
+                return [i+1, j+1]
+            elif numbers[i] + numbers[j] < target:
+                i += 1
+            else:
+                j -= 1
+
+Solution 2: using dictionary
 class Solution(object):
     def twoSum(self, numbers, target):
         """
